c0020cec30ec94e43e14abb941f35a13867f80c9
Eric Rabun <errabun@gmail.com>
2021-05-11 20:11:34 -0600

    Create App class component & movieData.js files

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,1 +1,13 @@
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
 
+import movieData from '../../movieData'
+
+class App extends Component {
+  constructor() {
+    super()
+    this.state:
+  }
+}
+
+export default App
