42a16f095ec5f95e8a6f93a46d3b556cb5d3db2c
Riley Martin <riley.willow@gmail.com>
2021-05-21 16:33:54 -0600

    Format MovieInfo page

--- src/components/MovieInfo/MovieInfo.js
+++ src/components/MovieInfo/MovieInfo.js
@@ -28,28 +28,31 @@ class MovieInfo extends Component {
     return (
       <>
         {!this.state.currentMovie &&
-          <h1>{this.state.error}</h1>}
+          <h1>{this.state.error}</h1>
+        }
+        {!this.state.currentMovie &&
+          !this.state.error &&
+          <h1>Loading..</h1>
+        }
         {this.state.currentMovie &&
         <section className='movie-card' style={{
           backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(${this.state.currentMovie.backdrop_path})`
         }}>
-          <div className='movie-display'>
-            <img src={this.state.currentMovie.poster_path} alt='movie poster' className='ind-movie-poster' />
-            <article className='movie-stats'>
-              <h2 className='title'>{this.state.currentMovie.title}</h2>
-              <h3 className='avg-rating'>Rating: {this.roundRating(this.state.currentMovie.average_rating)}</h3>
-              <p className='release-date'>Release Date: {this.state.currentMovie.release_date}</p>
-              <p className='overview'>Overview: {this.state.currentMovie.overview}</p>
-              <p className='genres'>Genres: {this.state.currentMovie.genres}</p>
-              <p className='budget'>Budget: ${this.state.currentMovie.budget}</p>
-              <p className='revenue'>Revenue: ${this.state.currentMovie.revenue}</p>
-              <p className='runtime'>Runtime: {this.state.currentMovie.runtime}</p>
-              <p className='tagline'>Tagline: {this.state.currentMovie.tagline}</p>
-              <Link to='/'>
-                <button className='return-home'>Return Home</button>
-              </Link>
-            </article>
-          </div>
+          <img src={this.state.currentMovie.poster_path} alt='movie poster' className='ind-movie-poster' />
+          <article className='movie-stats'>
+            <h2 className='title'>{this.state.currentMovie.title}</h2>
+            <h3 className='avg-rating'>Rating: {this.roundRating(this.state.currentMovie.average_rating)}</h3>
+            <p className='release-date'>Release Date: {this.state.currentMovie.release_date}</p>
+            <p className='overview'>Overview: {this.state.currentMovie.overview}</p>
+            <p className='genres'>Genres: {this.state.currentMovie.genres}</p>
+            <p className='budget'>Budget: ${this.state.currentMovie.budget}</p>
+            <p className='revenue'>Revenue: ${this.state.currentMovie.revenue}</p>
+            <p className='runtime'>Runtime: {this.state.currentMovie.runtime}</p>
+            <p className='tagline'>Tagline: {this.state.currentMovie.tagline}</p>
+            <Link to='/'>
+              <button className='return-home'>Return Home</button>
+            </Link>
+          </article>
         </section>}
       </>
     )
