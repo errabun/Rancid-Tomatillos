3b722f4941e99473dbdfc83a2bf994ff4a9fda25
Eric Rabun <errabun@gmail.com>
2021-05-22 12:38:37 -0600

    Create render and helper functions for search input

--- src/components/searchBar/searchBar.js
+++ src/components/searchBar/searchBar.js
@@ -1,19 +1,37 @@
-import React from 'react'
-import './searchBar.css' 
+import React, { Component } from 'react'
+import './searchBar.css'
 
-const Search = () => {
-  return (
-    <form>
-      <input
-        type='text'
-        placeholder='Search title or genre'
-        name='query'
-        value={}
-        onChange={event => filterSearch(event)} >  // need to write functionality for this function
-      </input>
-      <button onClick={event => submitSeach(event)}>SEARCH</button>
-    </form>
-  )
+class Search extends Component {
+  constructor() {
+    super()
+    this.state= {
+      query: ''
+    }
+  }
+
+  filterSearch = event => {
+    this.setState({ query: event.target.value })
+  }
+
+  submitSearch = event => {
+    event.preventDefault()
+
+  }
+
+  render() {
+    return (
+      <form>
+        <input
+          type='text'
+          placeholder='Search title or genre'
+          name='query'
+          value={this.state.query}
+          onChange={event => this.filterSearch(event)} >  // need to write functionality for this function
+        </input>
+        <button onClick={event => this.submitSearch(event)}>SEARCH</button>
+      </form>
+    )
+  }
 }
 
 export default Search
