dd48f14d492047512c5290438ba533aeda441c13
Eric Rabun <errabun@gmail.com>
2021-05-24 15:30:56 -0600

    Change file path to match GH file tree

--- src/components/AllMovies/AllMovies.js
+++ src/components/AllMovies/AllMovies.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import Movie from '../Movie/Movie'
-import Search from '../SearchBar/SearchBar'
+import Search from '../searchBar/searchBar'
 import './AllMovies.css'
 import { fetchAllMovies } from '../../utilities/ApiCalls'
 
--- src/components/searchBar/searchBar.js
+++ src/components/searchBar/searchBar.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import './SearchBar.css'
+import './searchBar.css'
 
 class Search extends Component {
   constructor(props) {
